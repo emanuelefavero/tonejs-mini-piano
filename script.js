6725a4fae82b3887a043d04898d19b6e8e14782e
emanuelefavero <emafaux89@gmail.com>
2023-01-26 00:11:45 +0100

    change code style in script.js

--- script.js
+++ script.js
@@ -1,151 +1,159 @@
 // CHECK IF MOBILE
-var isMobile = false; //initiate as false
+let isMobile = false //initiate as false
 // device detection
-if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
-    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
-    isMobile = true;
+if (
+  /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+    navigator.userAgent
+  ) ||
+  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+    navigator.userAgent.substr(0, 4)
+  )
+) {
+  isMobile = true
 }
 
 // IF NOT MOBILE, PLAY SOUND ON MOUSE CLICK
 if (isMobile === false) {
-    for (let i = 0; i < document.querySelectorAll(".key").length; i++) {
+  for (let i = 0; i < document.querySelectorAll('.key').length; i++) {
+    document.querySelectorAll('.key')[i].addEventListener('click', function () {
+      const buttonInnerHTML = this.textContent
 
-        document.querySelectorAll(".key")[i].addEventListener("click", function () {
+      makeSound(buttonInnerHTML)
 
-            const buttonInnerHTML = this.textContent;
+      buttonAnimation(buttonInnerHTML)
+    })
+  }
 
-            makeSound(buttonInnerHTML);
-
-            buttonAnimation(buttonInnerHTML);
-
-        });
-
-    }
-
-    // RELOAD BUTTON
-    const reset = document.querySelector('#reset').addEventListener('click', () => {
-        location.reload();
-    });
+  // RELOAD BUTTON
+  const reset = document
+    .querySelector('#reset')
+    .addEventListener('click', () => {
+      location.reload()
+    })
 } else if (isMobile === true) {
-    // PLAY SOUND ON TOUCH
-    for (let i = 0; i < document.querySelectorAll(".key").length; i++) {
+  // PLAY SOUND ON TOUCH
+  for (let i = 0; i < document.querySelectorAll('.key').length; i++) {
+    document
+      .querySelectorAll('.key')
+      [i].addEventListener('touchstart', function () {
+        const buttonInnerHTML = this.textContent
 
-        document.querySelectorAll(".key")[i].addEventListener("touchstart", function () {
+        makeSound(buttonInnerHTML)
 
-            const buttonInnerHTML = this.textContent;
+        buttonAnimation(buttonInnerHTML)
+      })
+  }
 
-            makeSound(buttonInnerHTML);
-
-            buttonAnimation(buttonInnerHTML);
-
-        });
-
-    }
-
-    // RELOAD BUTTON
-    const reset = document.querySelector('#reset').addEventListener('touchstart', () => {
-        location.reload();
-    });
-
+  // RELOAD BUTTON
+  const reset = document
+    .querySelector('#reset')
+    .addEventListener('touchstart', () => {
+      location.reload()
+    })
 }
 
 // PLAY SOUND ON KEYPRESS
-document.addEventListener("keypress", function (event) {
+document.addEventListener('keypress', function (event) {
+  makeSound(event.key)
 
-    makeSound(event.key);
-
-    buttonAnimation(event.key);
-
-});
+  buttonAnimation(event.key)
+})
 
 function makeSound(key) {
+  // SAMPLER
+  const sampler = new Tone.Sampler({
+    urls: {
+      C3: 'piano-C3.mp3',
+    },
+    release: 1,
+    baseUrl: 'sounds/',
+  })
 
-    // SAMPLER
-    const sampler = new Tone.Sampler({
-        urls: {
-            "C3": "piano-C3.mp3",
-        },
-        release: 1,
-        baseUrl: "sounds/",
-    });
+  // FX 1
+  const filterCutoff = document.querySelector('#cutoff').value
+  const fx1 = new Tone.Filter(filterCutoff, 'lowpass')
+  fx1.rolloff = '-48'
 
-    // FX 1
-    const filterCutoff = document.querySelector('#cutoff').value;
-    const fx1 = new Tone.Filter(filterCutoff, 'lowpass');
-    fx1.rolloff = '-48';
+  // FX 2
+  const delayTime = document.querySelector('#delayTime').value / 100
+  const delayFeedback = document.querySelector('#delayFeedback').value / 100
+  const fx2 = new Tone.FeedbackDelay(delayTime, delayFeedback)
 
-    // FX 2
-    const delayTime = document.querySelector('#delayTime').value / 100;
-    const delayFeedback = document.querySelector('#delayFeedback').value / 100;
-    const fx2 = new Tone.FeedbackDelay(delayTime, delayFeedback);
-
-    switch (key) {
-        case "a":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["C3"], 1).chain(fx1, fx2, Tone.Destination);
-            })
-            break;
-        case "s":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["D3"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        case "d":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["D#3"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        case "f":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["F3"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        case "g":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["G3"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        case "h":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["A#3"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        case "j":
-
-            Tone.loaded().then(() => {
-                sampler.triggerAttackRelease(["C4"], 1).chain(fx1, fx2, Tone.Destination)
-            })
-            break;
-        default:
-            console.log(key);
-    }
+  switch (key) {
+    case 'a':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['C3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 's':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['D3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 'd':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['D#3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 'f':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['F3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 'g':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['G3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 'h':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['A#3'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    case 'j':
+      Tone.loaded().then(() => {
+        sampler
+          .triggerAttackRelease(['C4'], 1)
+          .chain(fx1, fx2, Tone.Destination)
+      })
+      break
+    default:
+      console.log(key)
+  }
 }
 
 // KEY ANIMATIONS
 function buttonAnimation(currentKey) {
+  const activeButton = document.querySelector('.' + currentKey)
 
-    const activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add('key-pressed')
 
-    activeButton.classList.add("key-pressed");
-
-    setTimeout(function () {
-        activeButton.classList.remove("key-pressed");
-    }, 100);
+  setTimeout(function () {
+    activeButton.classList.remove('key-pressed')
+  }, 100)
 }
 
-const themeButton = document.querySelector('#themeButton').addEventListener('click', () => {
-    document.querySelector('main').classList.toggle('theme');
+const themeButton = document
+  .querySelector('#themeButton')
+  .addEventListener('click', () => {
+    document.querySelector('main').classList.toggle('theme')
 
     for (let i = 0; i < document.querySelectorAll('label').length; i++) {
-        document.querySelectorAll('label')[i].classList.toggle('theme');
+      document.querySelectorAll('label')[i].classList.toggle('theme')
     }
 
-    document.querySelector('section.keyboard').classList.toggle('invert');
-    document.querySelector('button.theme-button').classList.toggle('theme');
-});
+    document.querySelector('section.keyboard').classList.toggle('invert')
+    document.querySelector('button.theme-button').classList.toggle('theme')
+  })
